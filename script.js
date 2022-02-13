8cef2cc5cace3236493ddb9f085732a635873ccc
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-02-12 23:33:09 -0500

    Page reloading on URL change

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let templates = {};
 let app_div = document.getElementById('app');
 
 function home(){
+    reloadPage();
+
     let div = document.createElement('div');
     let link = document.createElement('a');
     link.href = '#/about';
@@ -16,6 +18,8 @@ function home(){
 };
 
 function about() {
+    reloadPage();
+    
     let div = document.createElement('div');
     let link = document.createElement('a');
     link.href = '#/';
@@ -25,49 +29,62 @@ function about() {
     div.appendChild(link);
 
     app_div.appendChild(div);
+}
 
-    function route (path, template) {
-        if (typeof template === 'function') {
-            return routes[path] = template;
-        }
-        else if (typeof template === 'string') {
-            return routes[path] = templates[template];
+function route (path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
+    }
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    }
+};
+    
+function template (name, templateFunction) {
+    return templates[name] = templateFunction;
+};
+    
+template('home', function(){
+    home();
+});
+    
+template('about', function(){
+    about();
+});
+    
+route('/', 'home');
+route('/about', 'about');
+    
+function resolveRoute(route) {
+    try {
+        return routes[route];
+    } catch (e) {
+        throw new Error(`Route ${route} not found`);
+    }
+};
+    
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let route = resolveRoute(url);
+    
+    route();
+};
+    
+window.addEventListener('load', router);
+    
+window.addEventListener('hashchange', router);
+
+function reloadPage(){
+    //get the actual local storage
+    if (window.localStorage) {
+        //retreive the "reload" item
+        if (!localStorage.getItem('reload')) {
+            localStorage['reload'] = true;
+            window.location.reload();
         } else {
-            return;
-        };
-    };
-    
-    function template (name, templateFunction) {
-        return templates[name] = templateFunction;
-    };
-    
-    template('home', function(){
-        home();
-    });
-    
-    template('about', function(){
-        about();
-    });
-    
-    route('/', 'home');
-    route('/about', 'about');
-    
-    function resolveRoute(route) {
-        try {
-            return routes[route];
-        } catch (e) {
-            throw new Error(`Route ${route} not found`);
-        };
-    };
-    
-    function router(evt) {
-        let url = window.location.hash.slice(1) || '/';
-        let route = resolveRoute(url);
-    
-        route();
-    };
-    
-    window.addEventListener('load', router);
-    
-    window.addEventListener('hashchange', router);
-}; 
+            localStorage.removeItem('reload');
+        }
+    }
+}
